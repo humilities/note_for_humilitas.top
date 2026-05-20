0d5afa2e923abe72cb9a95d434e9f2af99af051f
humilities <2099387470@qq.com>
2026-05-21 00:27:25 +0800

    fix the config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'algorithm', link: '/algorithm' }
+      { text: 'algorithm', link: '/algorithm' },
       { text: 'nvim', link: '/nvim'}
     ],
 
