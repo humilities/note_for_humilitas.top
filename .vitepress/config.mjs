f9a3bccd0dcab912aa8fb03a2fb68c274b2df794
humilities <2099387470@qq.com>
2026-05-21 08:43:59 +0800

    add the files into config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -16,7 +16,37 @@ export default defineConfig({
       {
         text: 'algoritm',
         items: [
-          { text: '26.3', link: '/algorithm/26.3' },
+          { text: '26.3',
+            link: '/algorithm/26.3/',
+            collapsed: true,
+            items:[
+              {text: '26.2.2.md',link: '/algorithm/26.3/26.2.2.md'}.
+              {text: '26.3.8.md',link: '/algorithm/26.3/26.3.8.md'}.
+              {text: '26.3.9.md',link: '/algorithm/26.3/26.3.9.md'}.
+              {text: '26.3.10.md',link: '/algorithm/26.3/26.3.10.md'}.
+              {text: '26.3.11.md',link: '/algorithm/26.3/26.3.11.md'}.
+              {text: '26.3.12.md',link: '/algorithm/26.3/26.3.12.md'}.
+              {text: '26.3.13.md',link: '/algorithm/26.3/26.3.13.md'}.
+              {text: '26.3.14.md',link: '/algorithm/26.3/26.3.14.md'}.
+              {text: '26.3.15.md',link: '/algorithm/26.3/26.3.15.md'}.
+              {text: '26.3.16.md',link: '/algorithm/26.3/26.3.16.md'}.
+              {text: '26.3.17.md',link: '/algorithm/26.3/26.3.17.md'}.
+            ]
+          }
+        ]
+      },
+      {
+        text: 'nvim'
+        link:'/nvim/',
+        collapsed:true,
+        items:[
+        {text:'the operation concerning tabs',link: "/nvim/the operation concerning tabs.md"},
+        {text:'the use of gitvim',link: "/nvim/the use of gitvim"},
+        {text:'my shortcut with <leader>',link: "/nvim/my shortcut with <leader>.md"},
+        {text:'the file operations based on the oil.nvim',link: "/nvim/the file operations based on the oil.nvim.md"},
+        {text:'the basic command',link: "/nvim/the basic command.md"},
+        {text:'the copy and paste in nvim',link: "/nvim/the copy and paste in nvim.md"},
+        {text:'the delete in nvim',link: "/nvim/the delete in nvim.md"}
         ]
       }
     ],
